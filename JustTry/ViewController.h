e4df5e71ede511d281e3d347ea041ca3c20d3cd0
蒋永昌 <jiang_yongchang@126.com>
2016-05-13 17:33:26 +0800

    Initial Commit

---
+++ JustTry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustTry
+//
+//  Created by 蒋永昌 on 16/5/13.
+//  Copyright © 2016年 蒋永昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JustTry/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JustTry
+//
+//  Created by 蒋永昌 on 16/5/13.
+//  Copyright © 2016年 蒋永昌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
